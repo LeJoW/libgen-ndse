e834559a6b2b725b9b9ad641099e4917e83fc312
LeJoW <elle.git.wilke@gmail.com>
2024-07-25 21:06:18 +0200

    corrections incipits

--- md2tex/config/incipits.ts
+++ md2tex/config/incipits.ts
@@ -1,10 +1,10 @@
 export const incipits: { [ps: string]: string } = {
     "53": "Deus, in nomine tuo salvum me fac",
     "109": "Dixit Dominus Domino meo",
-    "110": "Cofitebor tibi Domine",
+    "110": "Confitebor tibi Domine",
     "111": "Beatus vir qui timet Dominum",
     "112": "Laudate pueri Dominum",
-    "113": "In exitu Israel de Ægypto",
+    "113": "In exitu Israël de Ægypto",
     "118a": "Beati immaculati in via",
     "118b": "Retribue servo tuo, vivifica me",
     Magnificat: "Magnificat \\italic{(B. Mariæ Virg.)}",
