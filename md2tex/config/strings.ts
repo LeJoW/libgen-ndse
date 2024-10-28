f486a503b49d813564db531f5be45a4e1db0e5d2
LeJoW <elle.git.wilke@gmail.com>
2024-10-28 13:28:48 +0100

    commit

--- md2tex/config/strings.ts
+++ md2tex/config/strings.ts
@@ -36,13 +36,13 @@ const strConfig = (adapter: Adapter): StringConfigType => ({
     fr: [],
     all: [
         {
-            test: /\*{2}([^*]+)\*{2}/g,
+            test: /[\*_]{2}([^*]+?)[\*_]{2}/g,
             callback: function (_, text) {
                 return adapter.textStyles.bold(text);
             },
         },
         {
-            test: /[\*_]{1}([^*]+)[\*_]{1}/g,
+            test: /[\*_]{1}([^*]+?)[\*_]{1}/g,
             callback: function (_, text) {
                 return adapter.textStyles.italic(text);
             },
